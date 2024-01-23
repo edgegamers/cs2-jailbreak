c5363d1dc28e2b15f8b487775a984fe57e3f8bbd
MSWS <imodmaker@gmail.com>
2024-01-22 21:41:00 -0800

    Log grenades, fix damage logging

--- src/Warden/JailPlayer.cs
+++ src/Warden/JailPlayer.cs
@@ -327,7 +327,7 @@ public class JailPlayer
         }
         else
         {
-            JailPlugin.logs.AddLocalized(player, localKey, attacker!, damage);
+            JailPlugin.logs.AddLocalized(player, attacker!, localKey, damage);
         }
 
         if (isWorld)
